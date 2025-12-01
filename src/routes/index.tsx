8eabad019984cdb0d1523ca253bf42525aed310d
terawuth <132336480+terawuthlogicspark@users.noreply.github.com>
2025-12-01 16:42:43 +0700

    Refactor App styles and update headings for improved layout and readability

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,36 +1,36 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { RPConfig } from '@pdf-viewer/react'
-import { AppPdfViewer } from '../components/AppPdfViewer'
-import '../App.css'
+import { createFileRoute } from "@tanstack/react-router";
+import { RPConfig } from "@pdf-viewer/react";
+import { AppPdfViewer } from "../components/AppPdfViewer";
+import "../App.css";
 
-export const Route = createFileRoute('/')({
+export const Route = createFileRoute("/")({
   component: App,
-})
+});
 
 function App() {
   return (
     <div className="App">
       <RPConfig licenseKey="">
-      <div className="container">
-        <h1>RP Starter Toolkit: TanStack Router</h1>
-        <br />
-        <h2>Default Toolbar</h2>
-        <AppPdfViewer />
-        <h2>Without Toolbar</h2>
-        <AppPdfViewer
-          showToolbar={false}
-          defaultLayoutProps={{
-            style: { width: "100%", height: "550px" },
-          }}
-        />
-        <h2>Mobile</h2>
-        <AppPdfViewer
-          defaultLayoutProps={{
-            style: { width: "500px" },
-          }}
-        />
-      </div>
-    </RPConfig>
+        <div className="container">
+          <h1>RP Starter Toolkit: TanStack Router + TypeScript</h1>
+          <br />
+          <h2>Default Toolbar</h2>
+          <AppPdfViewer />
+          <h2>Without Toolbar</h2>
+          <AppPdfViewer
+            showToolbar={false}
+            defaultLayoutProps={{
+              style: { width: "100%", height: "550px" },
+            }}
+          />
+          <h2>Mobile</h2>
+          <AppPdfViewer
+            defaultLayoutProps={{
+              style: { width: "500px" },
+            }}
+          />
+        </div>
+      </RPConfig>
     </div>
-  )
+  );
 }
